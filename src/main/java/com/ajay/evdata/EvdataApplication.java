b1a7ab2721002a55c057d8957ef164f079c3f526
ajayinva@gmail.com <ajayinva@gmail.com>
2025-05-03 11:48:39 +0530

    Initial Commit

---
+++ src/main/java/com/ajay/evdata/EvdataApplication.java
@@ -0,0 +1,13 @@
+package com.ajay.evdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvdataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajay/evdata/EvdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajay.evdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
